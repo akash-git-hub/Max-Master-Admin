bbf5c8c2a1be1102f2ecf8a4b1fd6b45aabe2e30
akashwh <akash.rathore@wizehive.com>
2026-02-26 18:08:26 +0530

    module pages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Login from "./pages/Login";
 import Dashboard from "./pages/dashboard/Dashboard";
 import UniversityDetail from "./pages/university/UniversityDetail";
 import StudentProfile from "./pages/studentProfile/StudentProfile";
+import ModuleList from "./pages/modules/ModuleList";
+import CreateModule from "./pages/modules/CreateModule";
+import ModuleDetails from "./pages/modules/ModuleDetails";
 
 function App() {
   return (
@@ -13,6 +16,9 @@ function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/university-detail" element={<UniversityDetail />} />
         <Route path="/student-profile" element={<StudentProfile />} />
+        <Route path="/module-list" element={<ModuleList />} />
+        <Route path="/create-module" element={<CreateModule />} />
+        <Route path="/module-details" element={<ModuleDetails />} />
       </Routes>
     </div>
   );
