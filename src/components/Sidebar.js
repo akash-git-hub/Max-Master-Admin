328e351547755abb1a159b8e6c1550d8cfc854e9
Virendra Sahu <virendrasahu703@gmail.com>
2026-02-27 13:14:44 +0530

    create university API intigration

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -93,15 +93,15 @@ const Sidebar = ({ show, onClose }) => {
         <Nav.Link
           onClick={() => handleLinkClick("/university-list")}
           className={`d-flex align-items-center gap-3 px-4 py-3 rounded-5 ${pathname === "/university-list" || pathname === "/create-university"
-              ? "bg-dark text-white fw-semibold"
-              : "text-dark"
+            ? "bg-dark text-white fw-semibold"
+            : "text-dark"
             }`}
         >
           <ProjectIcon color={pathname === "/university-list" || pathname === "/create-university" ? "#fff" : "#292D32"} />
           University
         </Nav.Link>
 
-        <Nav.Link
+        {/* <Nav.Link
           onClick={() => handleLinkClick("/module-list")}
           className={`d-flex align-items-center gap-3 px-4 py-3 rounded-5 ${pathname.startsWith("/module-list")
               ? "bg-dark text-white fw-semibold"
@@ -110,7 +110,7 @@ const Sidebar = ({ show, onClose }) => {
         >
           <BoxIcon color={pathname.startsWith("/module-list") ? "#fff" : "#292D32"} />
           Modules
-        </Nav.Link>
+        </Nav.Link> */}
 
         <hr />
 
